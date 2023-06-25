8d9dab57f42d9ba32261a1c04fb542609d1e8957
Maximiliano <maximartins45@gmail.com>
2023-06-25 01:23:55 -0300

    Estructura del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="h-screen text-center flex flex-col justify-center">
+      <h1>Encuestas de reacciones para <span className="bg-sky-700 text-white font-semibold p-1" >LinkedIn</span></h1>
+    </div>
+  )
+}
+
+export default App
