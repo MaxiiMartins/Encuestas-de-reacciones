6090b4bb541fb2769007763b234ba2d71d304879
Maximiliano <maximartins45@gmail.com>
2023-06-30 16:30:07 -0300

    Modificando color del boton

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -13,9 +13,9 @@ const Download = () => {
   }
 
   return (
-    <div onClick={downloadImage}  className="cursor-pointer p-0.5 gradientAnimado rounded-full max-w-[180px] items-center bg-gradient-to-r group hover:from-[#FF7170] hover:to-[#FFE57F] from-white to-white transition-all  ease-linear">
+    <div onClick={downloadImage}  className="cursor-pointer p-0.5 gradientAnimado rounded-full max-w-[180px] items-center bg-gradient-to-r group from-[#70ff72] to-[#c8ff7f] transition-all  ease-linear">
       <div className="flex h-full w-full rounded-full items-center justify-center bg-[#0d1117] hover:bg-[#0d1117]/90">
-        <h1 className="font-semibold whitespace-nowrap z-0 px-4 py-2 text-xl text-transparent bg-clip-text bg-gradient-to-r group-hover:from-[#FF7170] group-hover:to-[#FFE57F] from-white to-white transition-all  ease-linear">Descargar</h1>
+        <h1 className="font-semibold whitespace-nowrap z-0 px-4 py-2 text-xl text-transparent bg-clip-text bg-gradient-to-r group-hover:from-[#70ff72] group-hover:to-[#c8ff7f] transition-all sease-linear">Descargar</h1>
       </div>
     </div>
   )
